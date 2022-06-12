7df04e76532796853be1b5c1609197e46daf0022
ali4real <alirasheed7122@gmail.com>
2022-06-12 13:15:49 -0700

    added tables in Schema[C

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -41,10 +41,27 @@ Payment_Status varchar(50),
 create table Product(
 Product_ID int primary key identity(1,1),
 Product_name varchar(50),
+Price int,
 
 )
 create table Inventory_control(
-Product_ID int primary key identity(1,1),
-Outlet_ID int,
+Product_ID int foreign key references Product(Product_ID),
+Outlet_ID int foreign key references Outlet(Outlet_ID),
+I_quantity int,
+
+)
+
+
+ create table Order_Placed(
+ Order_ID int primary key identity(1,1),
+ Payment_ID int foreign key references Payment_method(Payment_ID),
+
+ )
+
+create table Order_Details(
+ Order_ID int foreign key references Order_Placed(Order_ID),
+ Emp_ID int foreign key references Employee(Emp_ID),
+ Consumer_ID int foreign key references Consumer(Consumer_ID),
+ Payment_ID int foreign key references Payment_method(Payment_ID),
 
 )
