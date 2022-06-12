ef77ae2bdfdf2cebfc7dd684bd3ff2ff1c687382
ali4real <alirasheed7122@gmail.com>
2022-06-12 12:14:56 -0700

    added Metro in forms

--- PoS/PoS/Form1.Designer.cs
+++ PoS/PoS/Form1.Designer.cs
@@ -36,9 +36,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(51, 56);
+            this.button1.Location = new System.Drawing.Point(34, 36);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(158, 30);
+            this.button1.Size = new System.Drawing.Size(105, 20);
             this.button1.TabIndex = 0;
             this.button1.Text = "login as enterprise";
             this.button1.UseVisualStyleBackColor = true;
@@ -46,9 +47,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(51, 141);
+            this.button2.Location = new System.Drawing.Point(34, 92);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(158, 30);
+            this.button2.Size = new System.Drawing.Size(105, 20);
             this.button2.TabIndex = 1;
             this.button2.Text = "login as employee";
             this.button2.UseVisualStyleBackColor = true;
@@ -56,9 +58,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(51, 221);
+            this.button3.Location = new System.Drawing.Point(34, 144);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(158, 30);
+            this.button3.Size = new System.Drawing.Size(105, 20);
             this.button3.TabIndex = 2;
             this.button3.Text = "login as outlet";
             this.button3.UseVisualStyleBackColor = true;
@@ -66,9 +69,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(51, 307);
+            this.button4.Location = new System.Drawing.Point(34, 200);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(158, 30);
+            this.button4.Size = new System.Drawing.Size(105, 20);
             this.button4.TabIndex = 3;
             this.button4.Text = "login as customer";
             this.button4.UseVisualStyleBackColor = true;
@@ -76,15 +80,17 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 383);
+            this.ClientSize = new System.Drawing.Size(195, 249);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
