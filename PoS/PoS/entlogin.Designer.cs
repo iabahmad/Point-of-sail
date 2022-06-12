c8b6327cf4b19267c9eac21f65b5ebacaa6f0083
iabahmad <abdullahrashad3@gmail.com>
2022-06-13 01:25:42 +0500

    enterprise login created

--- PoS/PoS/entlogin.Designer.cs
+++ PoS/PoS/entlogin.Designer.cs
@@ -39,46 +39,41 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(149, 64);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Location = new System.Drawing.Point(224, 98);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(158, 20);
+            this.textBox1.Size = new System.Drawing.Size(235, 26);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(149, 119);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Location = new System.Drawing.Point(224, 183);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(158, 20);
+            this.textBox2.Size = new System.Drawing.Size(235, 26);
             this.textBox2.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(61, 68);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(92, 105);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.Size = new System.Drawing.Size(96, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "enterprise id";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(61, 123);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(92, 189);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.Size = new System.Drawing.Size(118, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "enterprise pass";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(243, 171);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(364, 263);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(63, 20);
+            this.button1.Size = new System.Drawing.Size(94, 31);
             this.button1.TabIndex = 4;
             this.button1.Text = "back";
             this.button1.UseVisualStyleBackColor = true;
@@ -86,20 +81,19 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(149, 171);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Location = new System.Drawing.Point(224, 263);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(63, 20);
+            this.button2.Size = new System.Drawing.Size(94, 31);
             this.button2.TabIndex = 5;
             this.button2.Text = "login";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(185, 194);
-            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Location = new System.Drawing.Point(278, 298);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(93, 20);
+            this.button3.Size = new System.Drawing.Size(140, 31);
             this.button3.TabIndex = 6;
             this.button3.Text = "forgot password";
             this.button3.UseVisualStyleBackColor = true;
@@ -107,9 +101,9 @@
             // 
             // entlogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(487, 250);
+            this.ClientSize = new System.Drawing.Size(730, 385);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -117,8 +111,8 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "entlogin";
+            this.Padding = new System.Windows.Forms.Padding(30, 92, 30, 31);
             this.Text = "entlogin";
             this.Load += new System.EventHandler(this.entlogin_Load);
             this.ResumeLayout(false);
