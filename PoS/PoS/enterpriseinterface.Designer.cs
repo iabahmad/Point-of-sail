956f00ae64812ea933dcc6fac08e626bf2bd413e
iabahmad <abdullahrashad3@gmail.com>
2022-06-13 09:28:19 +0500

    ent interfaces forms connected

--- PoS/PoS/enterpriseinterface.Designer.cs
+++ PoS/PoS/enterpriseinterface.Designer.cs
@@ -52,6 +52,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "add employee";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -61,6 +62,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "view outlets";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -70,6 +72,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "view employees";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // enterpriseinterface
             // 
